04560320b081ee43a8a0e6d951d2d473ebc13ab6
fox0430 <ipoduser0430@gmail.com>
2017-01-15 17:05:49 +0000

    nim note

---
+++ note.nim
@@ -0,0 +1,47 @@
+#Input
+
+var name = readLine(stdin)
+var a = stdin.readLine()
+var b = stdin.readLine.split.map(parseInt)
+
+#Output
+
+echo "Hello World"
+echo ("Hello World")
+
+#Do not new line 
+stdout.write "Hello "
+stdout.write "World"
+
+
+#variable,constance
+#variable
+var i:int
+echo i  #>>>0
+
+var x=1
+echo x  #>>>1
+
+var a,b=2
+echo a,b  #>>>22
+
+
+#constance ...needed number when declaration
+const C=1
+const I:int #>>>Error!!
+
+#let
+let I=1
+let m:int #>>>OK!
+
+#if sentence ...needed indentataion
+if x: x = false
+
+if x:
+  x=true
+elif y:
+  y=true
+else:
+  x=false
+
+
